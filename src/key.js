1bee6e4c884b2de0d723bd3db8b7a83121495978
Varsha Warungase <156581308+varsha-warungase03@users.noreply.github.com>
2026-01-14 21:41:44 +0530

    Update key.js

--- src/key.js
+++ src/key.js
@@ -1,4 +1,4 @@
 
 
 
-export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBhtYQnszYZ7_w1qIy4e92IIncJJtLPUJk";
+export const url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBOWNpVLoLHXJXtX1khfTBFa_E1QfUr-xk";
